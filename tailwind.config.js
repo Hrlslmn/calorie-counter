35aefac2270b59cf0652df9ea4c085609fe8ab8f
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-21 00:13:02 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,2 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    'node_modules/preline/src/*.js',
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  plugins: [],
-}
+
 
