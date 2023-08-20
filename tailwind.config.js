f9060248997bf37588fe1e2baaaad07655c3e25c
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-21 00:03:18 +0800

    sasxd

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     "./src/**/*.{html,js,jsx,ts,tsx}",
     'node_modules/preline/src/*.js',
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -21,6 +22,9 @@ module.exports = {
 =======
     "./src/**/*.{js,jsx,ts,tsx}",
 >>>>>>> parent of 7890054 (Revert "Revert "update"")
+=======
+    "./src/**/*.{js,jsx,ts,tsx}",
+>>>>>>> parent of cd9a7b3 (update)
 =======
     "./src/**/*.{js,jsx,ts,tsx}",
 >>>>>>> parent of cd9a7b3 (update)
