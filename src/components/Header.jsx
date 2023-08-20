1f7084356571375610a14ef99c192241e4999ae3
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-20 23:51:18 +0800

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,14 @@ function Header() {
     <div>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         <div className='relative'>
             <img src={header} alt="" className='border-b-4 border-yellow-400 w-full' />
             <div className='absolute md:bottom-[35%] md:left-[50px] left-[15px] bg-gray-800 bg-opacity-60 rounded-md bottom-[80px] md:mb-4'>
                 <h1 className='md:text-[25px] lg:text-[50px] md:p-[30px] p-2 text-sm text-white font-semibold'>Counting nutritions doesn't have to be complicated</h1>
 =======
+=======
+>>>>>>> parent of cd9a7b3 (update)
 =======
 >>>>>>> parent of cd9a7b3 (update)
         <div>
@@ -20,7 +23,10 @@ function Header() {
             <div>
                 <h1 className='font-serif'>Counting nutritions doesn't have to be complicated</h1>
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> parent of 7890054 (Revert "Revert "update"")
+=======
+>>>>>>> parent of cd9a7b3 (update)
 =======
 >>>>>>> parent of cd9a7b3 (update)
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   content: [
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     "./src/**/*.{html,js,jsx,ts,tsx}",
     'node_modules/preline/src/*.js',
     "./components/**/*.{js,ts,jsx,tsx}",
 =======
     "./src/**/*.{js,jsx,ts,tsx}",
 >>>>>>> parent of 7890054 (Revert "Revert "update"")
+=======
+    "./src/**/*.{js,jsx,ts,tsx}",
+>>>>>>> parent of cd9a7b3 (update)
 =======
     "./src/**/*.{js,jsx,ts,tsx}",
 >>>>>>> parent of cd9a7b3 (update)
