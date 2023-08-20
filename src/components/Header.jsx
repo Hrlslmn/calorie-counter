dfd7e1f420bccfe40e5fe923c5f1c41c0adfa41c
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-20 23:58:26 +0800

    s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,16 @@ import Navbar from './Navbar'
 function Header() {
   return (
     <div>
-<<<<<<< HEAD
         <div className='relative'>
             <img src={header} alt="" className='border-b-4 border-yellow-400' />
             <div className='absolute md:bottom-[35%] md:left-[50px] left-[15px] bg-gray-800 bg-opacity-60 rounded-md bottom-[80px] md:mb-4'>
                 <h1 className='md:text-[25px] lg:text-[50px] md:p-[30px] p-2 text-sm text-white font-semibold'>Counting nutritions doesn't have to be complicated</h1>
 
-=======
-        <div>
->>>>>>> parent of cd9a7b3 (update)
             <Navbar/>
         </div>
         <div>
             <div>
                 <h1 className='font-serif'>Counting nutritions doesn't have to be complicated</h1>
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of cd9a7b3 (update)
             </div>
             <div>
                 <button>Count Nutritions</button>
