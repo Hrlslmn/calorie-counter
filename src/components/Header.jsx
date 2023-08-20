d853c6d40464f8aac357d6a894ea5a8e7aa1fea1
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-20 23:56:58 +0800

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,42 +4,23 @@ import Navbar from './Navbar'
 function Header() {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         <div className='relative'>
             <img src={header} alt="" className='border-b-4 border-yellow-400' />
             <div className='absolute md:bottom-[35%] md:left-[50px] left-[15px] bg-gray-800 bg-opacity-60 rounded-md bottom-[80px] md:mb-4'>
                 <h1 className='md:text-[25px] lg:text-[50px] md:p-[30px] p-2 text-sm text-white font-semibold'>Counting nutritions doesn't have to be complicated</h1>
-=======
-=======
->>>>>>> parent of cd9a7b3 (update)
-=======
->>>>>>> parent of cd9a7b3 (update)
-=======
->>>>>>> parent of 7890054 (Revert "Revert "update"")
-        <div>
+
             <Navbar/>
         </div>
         <div>
             <div>
                 <h1 className='font-serif'>Counting nutritions doesn't have to be complicated</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 7890054 (Revert "Revert "update"")
-=======
->>>>>>> parent of cd9a7b3 (update)
-=======
->>>>>>> parent of cd9a7b3 (update)
-=======
->>>>>>> parent of 7890054 (Revert "Revert "update"")
+
             </div>
             <div>
                 <button>Count Nutritions</button>
             </div>
         </div>
+    </div>
     </div>
   )
 }
