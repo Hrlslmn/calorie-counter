3a9bdf398d460e37847597babeb41f137eaa49c4
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-20 23:50:52 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@
 module.exports = {
   content: [
 <<<<<<< HEAD
+<<<<<<< HEAD
     "./src/**/*.{html,js,jsx,ts,tsx}",
     'node_modules/preline/src/*.js',
     "./components/**/*.{js,ts,jsx,tsx}",
 =======
     "./src/**/*.{js,jsx,ts,tsx}",
 >>>>>>> parent of 7890054 (Revert "Revert "update"")
+=======
+    "./src/**/*.{js,jsx,ts,tsx}",
+>>>>>>> parent of cd9a7b3 (update)
   ],
   theme: {
     extend: {},
