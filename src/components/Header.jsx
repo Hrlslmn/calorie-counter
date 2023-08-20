1ea3385c4632d2d8e46273c08ff79d6d823e4534
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-21 00:22:51 +0800

    revert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div>
         <div className='relative'>
-            <img src={header} alt="" className='border-b-4 border-yellow-400' />
+            <img src={header} alt="" className='border-b-4 border-yellow-400 w-full' />
             <div className='absolute md:bottom-[35%] md:left-[50px] left-[15px] bg-gray-800 bg-opacity-60 rounded-md bottom-[80px] md:mb-4'>
                 <h1 className='md:text-[25px] lg:text-[50px] md:p-[30px] p-2 text-sm text-white font-semibold'>Counting nutritions doesn't have to be complicated</h1>
             </div>
