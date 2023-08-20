f92079f2d6a516f887d1edf8910834a5320e400b
Hairul Sulaiman <sulaimanhairul@gmail.com>
2023-08-20 23:49:11 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+<<<<<<< HEAD
     "./src/**/*.{html,js,jsx,ts,tsx}",
     'node_modules/preline/src/*.js',
     "./components/**/*.{js,ts,jsx,tsx}",
+=======
+    "./src/**/*.{js,jsx,ts,tsx}",
+>>>>>>> parent of 7890054 (Revert "Revert "update"")
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('preline/plugin'),
-  ],
+  plugins: [],
 }
 
