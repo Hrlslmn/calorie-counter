bba99546fcb00da8b07c6ef8e3c033d60d5f4a8b
hrlslmn <sulaimanhairul@gmail.com>
2023-08-23 21:31:48 +0800

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <div className='relative'>
             <img src={header} alt="" className='border-b-4 border-yellow-400 w-full' />
             <div className='absolute md:bottom-[35%] md:left-[50px] left-[15px] bg-gray-800 bg-opacity-60 rounded-md bottom-[80px] md:mb-4'>
-                <h1 className='md:text-[25px] lg:text-[50px] md:p-[30px] p-2 text-sm text-white font-semibold'>Counting nutritions doesn't have to be complicated</h1>
+                <h1 className='md:text-[25px] lg:text-[40px] md:p-[30px] p-2 text-sm text-white font-semibold'>Counting nutritions doesn't have to be complicated</h1>
             </div>
             <div className='absolute md:bottom-[25%] bottom-6 md:left-[50px] left-[15px]'>
                 <button className='bg-blue-700 p-2 rounded-md hover:rounded-xl hover:bg-blue-400 duration-300 border-yellow-400 border-2 md:text-lg text-sm font-semibold text-white'><Link to="/counter">Count Nutritions</Link></button>
